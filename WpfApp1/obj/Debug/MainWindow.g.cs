89a86cf3cd6e34faa7bc5dda2a2433cd6737347a
Yauheni Unger <yauheni_unger@epam.com>
2018-02-09 18:37:10 +0300

    removed junk nuget packages

--- WpfApp1/obj/Debug/MainWindow.g.cs
+++ WpfApp1/obj/Debug/MainWindow.g.cs
@@ -30,21 +30,6 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Windows.Shell;
 using WpfApp1;
-using Xceed.Wpf.Toolkit;
-using Xceed.Wpf.Toolkit.Chart;
-using Xceed.Wpf.Toolkit.Chromes;
-using Xceed.Wpf.Toolkit.Core.Converters;
-using Xceed.Wpf.Toolkit.Core.Input;
-using Xceed.Wpf.Toolkit.Core.Media;
-using Xceed.Wpf.Toolkit.Core.Utilities;
-using Xceed.Wpf.Toolkit.Panels;
-using Xceed.Wpf.Toolkit.Primitives;
-using Xceed.Wpf.Toolkit.PropertyGrid;
-using Xceed.Wpf.Toolkit.PropertyGrid.Attributes;
-using Xceed.Wpf.Toolkit.PropertyGrid.Commands;
-using Xceed.Wpf.Toolkit.PropertyGrid.Converters;
-using Xceed.Wpf.Toolkit.PropertyGrid.Editors;
-using Xceed.Wpf.Toolkit.Zoombox;
 
 
 namespace WpfApp1 {
